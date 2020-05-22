91d8c3dc84cb6ea3f3239a44c611cc90df4a3e2a
fabrimaz <ing.mazzone.fabrizio@gmail.com>
2020-05-22 12:14:34 +0200

    Fabrizio_version_2
    - matching between landmarks and boxes modified (big changes in enconding.py)
    - Added new logics to use precomputed encodings
    - TODO completed

--- face_recognition_prod/main.py
+++ face_recognition_prod/main.py
@@ -6,6 +6,7 @@ from src.authenticator import FaceAuthenticator
 
 WINDOW_NAME = "Face Authentication"
 
+PRECOMPUTED_ENCS = False
 
 def run():
     # init video feed
@@ -15,41 +16,41 @@ def run():
 
     # or like this if we want use a saved video
     # video_capture = cv2.VideoCapture(video path)
-    face_encoder = FaceEncoder()
+    
     #---------------------------------------------------------#
-    #!!!!!!!!!!!!!!!!!!!CHANGE THE PATH!!!!!!!!!!!!!!!!!!!!!!!!!
-
-    # TODO: why are we still referring to set A?
-    path = 'path of the encodings of the set A'
-    face_authenticator = FaceAuthenticator(path)
+    ref_path = 'C:/Users/super/Jupypter/#11/encodings_88/fa.pkl'
+    input_path = "C:/Users/super/Jupypter/#11/encodings_88/matti.pkl"
+    face_encoder = FaceEncoder(PRECOMPUTED_ENCS, input_path)
+    face_authenticator = FaceAuthenticator(ref_path)
     #---------------------------------------------------------#
 
     while True:
-        ret, frame = video_capture.read()
-        frame = compress(frame, 2)  # to make it run faster
+        if not PRECOMPUTED_ENCS:
+            ret, frame = video_capture.read(0)
+            frame = compress(frame, 2)  # to make it run faster
+        else:
+            ret = True
+            frame = None
 
         if not ret:
             break
 
         # run the face tracker
-        feedback, encoding = face_encoder.run(frame)
+        face_detected, encoding = face_encoder.run(frame)
         # if no encodings are detected use the next frame
-        # TODO: you can be more precise with the statement by only allowing to run face_authenticator if feedback
-        # has certain value
-        if not feedback:
-            continue
-        # run face authenticator
-        # TODO: try to find more explicit names for feedback and response, like this it is hard to understand which does what
-        feedback, response = face_authenticator.run(encoding)
+        if face_detected:
+            # run face authenticator
+            stop_recognition, final_answer = face_authenticator.run(encoding)
 
-        # if a final answer is given stop the recognition
-        # TODO: a bit misleading that feedback is also the output of face_encoder.
-        if feedback:
-            print(response)#True or False based on the fact that the person is recognized or not
-            break
+            # if a final answer is given stop the recognition
+            if stop_recognition:
+                # True or False on the fact that the person is recognized or not
+                print(final_answer)
+                break
 
         # show frames
-        cv2.imshow(WINDOW_NAME, frame)
+        if not PRECOMPUTED_ENCS:
+            cv2.imshow(WINDOW_NAME, frame)
 
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
